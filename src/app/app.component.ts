1bab9d44aed0a9615f4ed6abe4463fce33f0a70f
Jon Altan <jon@groovygecko.com>
2024-01-23 14:48:47 +0000

    setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as THEOplayer from 'theoplayer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit {
   title = 'theo650';
+  @ViewChild('playerRef') playerElementRef!: ElementRef;
+
+
+  ngAfterViewInit(): void {
+    const player = new THEOplayer.Player(this.playerElementRef.nativeElement, {});
+  }
+
 }
